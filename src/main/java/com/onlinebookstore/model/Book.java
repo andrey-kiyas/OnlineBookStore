bdead53afb666119761070e39ec8fc0f1f35b05a
ANDREY <andrey.kiyas@gmail.com>
2023-08-07 12:18:33 +0300

    add-book-entity-service-repository (checkstyle)

--- src/main/java/com/onlinebookstore/model/Book.java
+++ src/main/java/com/onlinebookstore/model/Book.java
@@ -3,11 +3,11 @@ package com.onlinebookstore.model;
 import java.math.BigDecimal;
 
 public class Book {
-    private Long id;                //    id (Long, PK)
-    private String title;           //    title (String, not null)
-    private String author;          //    author (String, not null)
-    private String isbn;            //    isbn (String, not null, unique)
-    private BigDecimal price;       //    price (BigDecimal, not null)
+    private Long id; // id (Long, PK)
+    private String title; // title (String, not null)
+    private String author; // author (String, not null)
+    private String isbn; // isbn (String, not null, unique)
+    private BigDecimal price; // price (BigDecimal, not null)
     private BigDecimal description;
     private BigDecimal coverImage;
 }
