5e6cebae8e36bba93412ad81e1f7365af8127d52
ANDREY <andrey.kiyas@gmail.com>
2023-08-13 22:25:16 +0300

    add-jparepository-liquibase-ddlvalidate-softdelete-endpoints (pom - Leonid Sivko)

--- src/main/java/com/onlinebookstore/dto/BookSearchParameters.java
+++ src/main/java/com/onlinebookstore/dto/BookSearchParameters.java
@@ -3,10 +3,3 @@ package com.onlinebookstore.dto;
 public record BookSearchParameters(String[] title, String[] author, String[] isbn, String[] price,
                                    String[] description) {
 }
-
-
-
-
-
-
-
