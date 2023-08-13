f5c876ad1df108d2f5c27e79fc12893b19c24d66
ANDREY <andrey.kiyas@gmail.com>
2023-08-13 17:04:27 +0300

    add-jparepository-liquibase-ddlvalidate-softdelete-endpoints (Anastasia Len)

--- src/main/java/com/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/com/onlinebookstore/mapper/BookMapper.java
@@ -5,10 +5,16 @@ import com.onlinebookstore.dto.BookDto;
 import com.onlinebookstore.dto.CreateBookRequestDto;
 import com.onlinebookstore.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "deleted", ignore = true)
+    })
     Book toModel(CreateBookRequestDto requestDto);
 }
