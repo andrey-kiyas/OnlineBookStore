176c5dbc62d5eba20870cc56f7c2571cfcefcc6c
ANDREY <andrey.kiyas@gmail.com>
2023-08-28 12:14:20 +0300

    08-shopping_cart-cart_item-shopping_cart_management (Leonid Sivko)

--- src/main/java/com/onlinebookstore/mapper/CartItemMapper.java
+++ src/main/java/com/onlinebookstore/mapper/CartItemMapper.java
@@ -2,6 +2,7 @@ package com.onlinebookstore.mapper;
 
 import com.onlinebookstore.config.MapperConfig;
 import com.onlinebookstore.dto.cartitem.CartItemRequestDto;
+import com.onlinebookstore.dto.cartitem.CartItemResponseDto;
 import com.onlinebookstore.dto.cartitem.CartItemUpdateDto;
 import com.onlinebookstore.model.Book;
 import com.onlinebookstore.model.CartItem;
@@ -9,6 +10,7 @@ import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Mappings;
 
 @Mapper(config = MapperConfig.class)
 public interface CartItemMapper {
@@ -17,6 +19,12 @@ public interface CartItemMapper {
 
     @Mapping(target = "book", ignore = true)
     CartItem toEntity(CartItemUpdateDto cartItemUpdateDto);
+
+    @Mappings({
+            @Mapping(target = "bookId", source = "book.id"),
+            @Mapping(target = "bookTitle", source = "book.title")
+    })
+    CartItemResponseDto toDto(CartItem cartItem);
 
     @AfterMapping
     default void setBookId(@MappingTarget CartItem cartItem,
--- src/main/java/com/onlinebookstore/mapper/ShoppingCartMapper.java
+++ src/main/java/com/onlinebookstore/mapper/ShoppingCartMapper.java
@@ -1,37 +1,18 @@
 package com.onlinebookstore.mapper;
 
 import com.onlinebookstore.config.MapperConfig;
-import com.onlinebookstore.dto.cartitem.CartItemResponseDto;
 import com.onlinebookstore.dto.shoppingcart.ShoppingCartResponseDto;
-import com.onlinebookstore.model.CartItem;
 import com.onlinebookstore.model.ShoppingCart;
-import java.util.HashSet;
-import java.util.Set;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.Mapping;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, uses = CartItemMapper.class)
 public interface ShoppingCartMapper {
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "cartItems", target = "cartItems")
     ShoppingCartResponseDto toDto(ShoppingCart shoppingCart);
 
+    @Mapping(source = "userId", target = "user.id")
+    @Mapping(source = "cartItems", target = "cartItems")
     ShoppingCart toEntity(ShoppingCartResponseDto shoppingCartResponseDto);
-
-    @AfterMapping
-    default void setShoppingCartResponseDtoParams(
-            @MappingTarget ShoppingCartResponseDto shoppingCartResponseDto,
-            ShoppingCart shoppingCart
-    ) {
-        shoppingCartResponseDto.setUserId(shoppingCart.getUser().getId());
-        Set<CartItemResponseDto> cartItemSet = new HashSet<>();
-        for (CartItem item : shoppingCart.getCartItems()) {
-            CartItemResponseDto cartItem = new CartItemResponseDto();
-            cartItem.setId(item.getId());
-            cartItem.setBookId(item.getBook().getId());
-            cartItem.setBookTitle(item.getBook().getTitle());
-            cartItem.setQuantity(item.getQuantity());
-            cartItemSet.add(cartItem);
-        }
-        shoppingCartResponseDto.setCartItems(cartItemSet);
-    }
 }
